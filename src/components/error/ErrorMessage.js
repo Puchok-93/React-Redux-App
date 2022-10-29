7632ab302a7ece5455fe07ca7a43b6e90082ce79
Puchok-93 <54364298+Puchok-93@users.noreply.github.com>
2022-10-30 01:31:53 +0300

    Добавление спиннера и обработка ошибки

---
+++ src/components/error/ErrorMessage.js
@@ -0,0 +1,10 @@
+import img from './error.gif';
+
+const ErrorMessage = () => {
+    return (
+        <img src={img} style={{ display: 'block', width: '250px', height: '250px', objectFit: 'contain', margin: '0 auto'}} alt='Ошибка'/>
+        // <img src={process.env.PUBLIC_URL + '/error.gif'}/> Вариант использования картинки в public
+    )
+}
+
+export default ErrorMessage;
